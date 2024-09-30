2ce8f3345dfc83f56987352be36ae428ed239a0b
seonghuncode <gnos456@naver.com>
2024-09-30 17:47:42 +0900

    사진 관련 내용 수정 완료

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,62 +1,82 @@
-import image01 from '@/assets/images/01.jpg'
-import image02 from '@/assets/images/02.jpg'
-import image03 from '@/assets/images/03.jpg'
-import image04 from '@/assets/images/04.jpg'
-import image05 from '@/assets/images/05.jpg'
-import image06 from '@/assets/images/06.jpg'
-import image07 from '@/assets/images/07.jpg'
-import image08 from '@/assets/images/08.jpg'
-import image09 from '@/assets/images/09.jpg'
-import image10 from '@/assets/images/10.jpg'
-import image11 from '@/assets/images/11.jpg'
-import image12 from '@/assets/images/12.jpg'
+// import image01 from '@/assets/images/01.jpg'
+// import image02 from '@/assets/images/02.jpg'
+// import image03 from '@/assets/images/03.jpg'
+// import image04 from '@/assets/images/04.jpg'
+// import image05 from '@/assets/images/05.jpg'
+// import image06 from '@/assets/images/06.jpg'
+// import image07 from '@/assets/images/07.jpg'
+// import image08 from '@/assets/images/08.jpg'
+// import image09 from '@/assets/images/09.jpg'
+// import image10 from '@/assets/images/10.jpg'
+// import image11 from '@/assets/images/11.jpg'
+// import image12 from '@/assets/images/12.jpg'
+
+import image1 from '@/assets/images/cave1.jpeg'
+import image2 from '@/assets/images/cave2.jpeg'
+import image3 from '@/assets/images/cave3.jpeg'
+
+import image4 from '@/assets/images/bike1.png'
+import image5 from '@/assets/images/bike2.jpeg'
+import image6 from '@/assets/images/bike3.jpeg'
+
+import image7 from '@/assets/images/park1.jpeg'
+import image8 from '@/assets/images/park2.jpeg'
+import image9 from '@/assets/images/park3.jpeg'
+
+import image10 from '@/assets/images/home1.jpeg'
+import image11 from '@/assets/images/home2.jpeg'
+import image12 from '@/assets/images/home3.jpeg'
+
+import image13 from '@/assets/images/spa1.png'
+import image14 from '@/assets/images/spa2.jpeg'
+import image15 from '@/assets/images/spa3.jpeg'
 
 const images = [   /* 이미지 경로를 리스트로 저장 */
   {
-    alt: 'image01',
-    source: image01,
+    alt: 'image1',
+    source: image1,
     width: 640,
     height: 960
   },
   {
-    alt: 'image02',
-    source: image02,
+    alt: 'image2',
+    source: image2,
     width: 640,
     height: 960
   },
   {
-    alt: 'image03',
-    source: image03,
+    alt: 'image3',
+    source: image3,
     width: 640,
     height: 960
   },{
-    alt: 'image04',
-    source: image04,
+    alt: 'image4',
+    source: image4,
     width: 640,
     height: 960
   },{
-    alt: 'image05',
-    source: image05,
+    alt: 'image5',
+    source: image5,
     width: 640,
     height: 960
   },{
-    alt: 'image06',
-    source: image06,
+    alt: 'image6',
+    source: image6,
     width: 640,
     height: 960
   },{
-    alt: 'image07',
-    source: image07,
+    alt: 'image7',
+    source: image7,
     width: 640,
     height: 960
   },{
-    alt: 'image08',
-    source: image08,
+    alt: 'image8',
+    source: image8,
     width: 640,
     height: 960
   },{
-    alt: 'image09',
-    source: image09,
+    alt: 'image9',
+    source: image9,
     width: 640,
     height: 960
   },{
@@ -71,6 +91,21 @@ const images = [   /* 이미지 경로를 리스트로 저장 */
     height: 960
   },{
     alt: 'image12',
+    source: image12,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image13',
+    source: image10,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image14',
+    source: image11,
+    width: 640,
+    height: 960
+  },{
+    alt: 'image15',
     source: image12,
     width: 640,
     height: 960
